2ea206f63c49b681e4f34bd4f1e27ccfad930bf0
Olivia Osborn <olioz95@gmail.com>
2019-02-26 16:41:51 -0500

    set up cookie

--- server.js
+++ server.js
@@ -4,8 +4,22 @@ const helmet = require("helmet")
 const server = express();
 const Users = require("./users/users-module");
 
+const sessionConfig = {
+    name: "cookieName",
+    secret: "random secret",
+    cookie: {
+        maxAge: 1000 * 60 * 60, //in ms
+        secure: false,
+    },
+    httpOnly: true,
+    resave: false,
+    saveUninitialized: false,
+}
+
+//middleware: 
 server.use(helmet());
 server.use(express.json());
+server.use(session(sessionConfig));
 
 server.post("/api/register", (req, res) => {
     let user = req.body;
@@ -28,6 +42,7 @@ server.post("/api/login", (req, res) => {
         .first()
         .then(user => {
             if (user && bcrypt.compareSync(password, user.password)) {
+                req.session.user = user;
                 res.status(200).json({message: `Welcome ${user.username}`})
             } else {
                 res.status(401).json({message: "Invalid credentials!!"})
@@ -38,26 +53,13 @@ server.post("/api/login", (req, res) => {
         });
 });
 
-function restricted(req, res, next) {
-    const { username, password } = req.headers;
-  
-    if (username && password) {
-      Users.getBy({ username })
-        .first()
-        .then(user => {
-          if (user && bcrypt.compareSync(password, user.password)) {
-            next();
-          } else {
-            res.status(401).json({ message: 'Invalid Credentials' });
-          }
-        })
-        .catch(error => {
-          res.status(500).json({ message: 'Ran into an unexpected error' });
-        });
+function restricted( req, res, next) {
+    if (req.session && req.session.user) {
+        next();
     } else {
-      res.status(400).json({ message: 'No credentials provided' });
+        res.status(401).json({message: "you're not allowed to be here!"})
     }
-  }
+}
 
   server.get('/api/users', restricted, (req, res) => {
     Users.get()
